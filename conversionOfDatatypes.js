b9c8a4e0b7013d6c82db01ef9ac8789bebe3f775
Swati Badola <102805192+swatibadola@users.noreply.github.com>
2023-11-25 09:25:18 +0000

    datatypes

---
+++ conversionOfDatatypes.js
@@ -0,0 +1,31 @@
+let score = "12jki"
+// let score = undefined
+// let score = null
+
+
+console.log(score);
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof(valueInNumber))
+
+
+// "45" => 45
+// "45dhb" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = "swati"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"swati" =>true
+
+let num = 46
+
+let stringNumber = String(num)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
