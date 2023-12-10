6767cd78a4c8efc88c5eca49d310e0b145350335
Swati Badola <102805192+swatibadola@users.noreply.github.com>
2023-12-10 13:23:03 +0000

    two

---
+++ iterations/two.js
@@ -0,0 +1,27 @@
+// WHILE LOOP
+
+let index = 0
+while(index <= 4){
+    // console.log(`Value of index is ${index}`)
+    index = index + 1
+}
+
+
+let array = ["London", "Paris", "Amsterdam"]
+let arr = 0
+
+while(arr < array.length){
+    // console.log(`Value is ${array[arr]}`)
+    arr++
+}
+
+
+
+// DO-WHILE LOOP  => Gets printed at least 1 time even if the condition is not satisfied
+let score = 11
+
+do{
+    console.log(`Score is ${score}`)
+    score++
+}
+while(score <= 10)
