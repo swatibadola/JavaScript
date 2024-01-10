4905c57d1ff52c6e7d6baa4c8538e13d3bf4d4ba
Swati Badola <102805192+swatibadola@users.noreply.github.com>
2024-01-10 11:02:24 +0000

    project01

---
+++ Projects/project01.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach(function (buttton)
+{
+    console.log(button);
+})
---
+++ Projects/project01.md
@@ -0,0 +1,13 @@
+#Projects related to DOM
+
+##Project link
+[Click here]
+
+#Solution Code
+#Project 1
+
+
+```javascript
+console.log("Swati badola");
+
+```  
