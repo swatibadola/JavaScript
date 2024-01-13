845ffb9fd51391e8321f986d20b89b84132f32ca
Swati Badola <102805192+swatibadola@users.noreply.github.com>
2024-01-13 08:20:04 +0000

    index

---
+++ Projects/BMIGenerator/project02.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form')
+//this usecase will give empty
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const result = document.querySelector('#result')
+}) 
