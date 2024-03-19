f136a42a3a16b984c370b528c9a8f204da8b45d5
Swati Badola <102805192+swatibadola@users.noreply.github.com>
2024-03-19 16:39:09 +0000

    test

--- NEW_CONCEPTS_js/test.js
+++ NEW_CONCEPTS_js/test.js
@@ -28,3 +28,31 @@ arrTwo[10] = 11
 console.log(arrTwo);
 console.log(arrTwo.length);
 console.log(arrTwo[9]);
+
+
+//bound check - to check if any holes exist in the array
+// hasOwnProperty(arrTwo, 9)
+// hasOwnProperty(arrTwo.prototype, 10)
+// hasOwnProperty(Object.prototype, 10)
+
+
+// OPTIMIZATION ORDER
+// SMI > DOUBLE > PACKED
+// HOLEY_SMI > HOLEY_DOUBLE > H_PACKED
+
+
+const arrThree = new Array(3)
+// just 3 holes. HOLEY_SMI_ELEMENTS
+arrThree[0] = '1' //HOLEY_ELEMENTS
+arrThree[1] = '2' //HOLEY_ELEMENTS
+arrThree[2] = '3' //HOLEY_ELEMENTS
+
+
+const arrFour = []
+arrFour.push('1') //PACKED_ELEMENTS
+arrFour.push('2') //PACKED_ELEMENTS
+arrFour.push('3') //PACKED_ELEMENTS
+
+
+const arrFive = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+arrFive.push(infinity)
